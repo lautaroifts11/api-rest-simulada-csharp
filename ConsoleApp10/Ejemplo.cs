8446a4782184e8d3a73f7425a4aae21665133527
cybergausho <lauttmz@gmail.com>
2026-06-08 19:23:00 -0300

    primer commit amigos

---
+++ ConsoleApp10/Ejemplo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Ejemplo
+    {
+        // C# easy mode
+        public int id;
+        public string name;
+
+
+        //agnostic
+        public int getId()
+        {
+            return this.id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+    }
+}
